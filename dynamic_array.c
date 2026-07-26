22433ae635b7b108d08c97f6f5c5710404fc5b9a
Antonio <giovanniangotti890@gmail.com>
2026-07-27 01:33:15 +0200

    Implemented FIFO and LIFO polic methods as well as find and delete_at methods

--- dynamic_array.c
+++ dynamic_array.c
@@ -21,6 +21,29 @@ DynamicArray* create_array (int initial_capacity){
 	return arr; //array successufully created
 }//create_array
 
+/*returns idx of first 'value' occurrency, this function returns -1 upon not found instance */
+int array_find (DynamicArray *arr, int value){
+	for (int i = 0; i<arr->size; i++){
+		if (arr->data[i] == value) return i;
+	}
+	return -1;
+}//array_find
+
+int array_delete_at (DynamicArray *arr, int idx){
+	if (idx < 0 || idx >= arr->size){
+		printf("Index out of bound\n");
+		exit(1);
+	}
+	if (idx == arr->size -1)return array_pop(arr);
+	else if(idx == 0) return array_dequeue(arr);
+	else {
+		int tmp = arr->data[idx];
+		for (int i = idx; i<arr->size -1; i++) arr->data[i] = arr->data[i+1];
+		arr->size--;
+		return tmp;
+	}
+}//array_delete_at
+
 /*appends value 'value' at the tail */
 void array_append (DynamicArray *arr, int value){
 	if (arr->size >= arr->capacity){
@@ -44,6 +67,54 @@ int array_get (DynamicArray *arr, int idx){
 	}
 	return arr->data[idx];
 }//array_get
+
+//start of LIFO policy methods
+
+/*same as array_append (DynamicArray *arr, int value) renaming for LIFO policy*/
+void array_push(DynamicArray *arr, int value){
+	array_append(arr, value);
+}//array_push
+
+/* returns last element stored in arr->data and deletes it*/
+int array_pop(DynamicArray *arr){
+	if (arr->size == 0){
+		printf("Can't pop from empty stack\n");
+		exit(1);
+	}
+	return arr->data[--arr->size];
+}//array_pop
+
+/* returns last element stored in arr->data without altering it */
+int array_peek(DynamicArray *arr){
+	if (arr->size == 0){
+		printf("Can't peek from an empty stack\n");
+		exit(1);
+	}
+	return arr->data[arr->size -1];
+}//array_peek
+
+//end of LIFO policy methods
+
+//start of FIFO policy methods
+
+/*same as array_append (DynamicArray *arr, int value) renaming for FIFO policy*/
+void array_enqueue(DynamicArray *arr, int value){
+	array_append(arr,value);
+}//array_enqueue
+
+/*returns current head of arr->data and deletes it */
+int array_dequeue(DynamicArray *arr){
+	if (arr->size == 0){
+		printf("Can't dequeue from empty queue\n");
+		exit(1);
+	}
+	int tmp = arr->data[0];
+	for (int i = 0; i<arr->size -1; i++) arr->data[i]=arr->data[i+1];
+	arr->size--;
+	return tmp;
+}//array_dequeue
+
+//end of FIFO policy methods
 
 /*frees all the allocated memory to avoid memory leak */
 void free_array (DynamicArray *arr){
--- dynamic_array.h
+++ dynamic_array.h
@@ -16,8 +16,22 @@ int capacity; //number of total cells in the array
 //functions signatures
 
 DynamicArray* create_array(int initial_capacity); //such as the constructor in OOP languages
+int array_find(DynamicArray *arr, int value); //returns idx of first 'value' occurency
+int array_delete_at(DynamicArray *arr, int idx);//deletes the value stored at arr->data[idx]
 void array_append (DynamicArray *arr, int value); //appends in tail the value 'value'
 int array_get(DynamicArray *arr, int idx); //gets value stored at arr[idx]
+
+//start of LIFO policy methods
+void array_push(DynamicArray *arr, int value);//pushes value, LIFO policy
+int array_pop(DynamicArray *arr);//pops last value from the array, LIFO policy
+int array_peek(DynamicArray *arr); //returns next value that will be popped without deleting it
+//end of LIFO policy methods
+
+//start of FIFO policy methods
+void array_enqueue (DynamicArray *arr, int value);//queues the value 'value', appending to tail, FIFO policy
+int array_dequeue (DynamicArray *arr);//returns current head and deletes it, FIFO policy
+//end of LIFO policy methods
+
 void free_array(DynamicArray *arr); //frees the memory destroying the array
 void clear(DynamicArray *arr); //clears array
 char *toString(DynamicArray *arr);//toString method
